8108748dd22baa604ea892c30135dc178aa53326
nicolasperussi <nicolasperussi12@hotmail.com>
2023-10-11 21:56:46 -0300

    feat: create UserController

---
+++ src/main/java/com/library/controllers/UserController.java
@@ -0,0 +1,24 @@
+package com.library.controllers;
+
+import com.library.domain.User;
+import com.library.services.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping(value = "/users")
+public class UserController {
+    @Autowired
+    private UserService service;
+
+    @GetMapping
+    public ResponseEntity<List<User>> findAll() {
+        List<User> list = service.findAll();
+        return ResponseEntity.ok().body(list);
+    }
+}
